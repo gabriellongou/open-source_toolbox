d676026588b6d5387812cc93f25afdbb5ead9b8d
gabriellongou <100798152+gabriellongou@users.noreply.github.com>
2023-10-24 19:09:07 +0200

    Create a loop over all the 30 min segments

--- speech-to-text/cut.py
+++ speech-to-text/cut.py
@@ -2,23 +2,35 @@ from pydub import AudioSegment
 
 try:
     # Load the .m4a file
-    song = AudioSegment.from_file("my_full_audio.m4a", format="m4a")
+    song = AudioSegment.from_file("my_full_audio_file.m4a", format="m4a")
 
-    # Check the length of the song
-    song_length = len(song) / (60 * 1000)  # Convert to minutes
-    print(f"Original song length: {song_length} minutes")
+    # Check the length of the file in milliseconds
+    song_length_ms = len(song)
+    song_length_minutes = song_length_ms / (60 * 1000)  # Convert to minutes
+    print(f"Original song length: {song_length_minutes:.2f} minutes")
 
     # PyDub handles time in milliseconds
     thirty_minutes = 30 * 60 * 1000
 
-    # Extract audio from 30 minutes until the end
-    after_30_minutes = song[thirty_minutes:]
+    # Initialize segment counters
+    segment_start = 0
+    segment_end = thirty_minutes
+    segment_counter = 1
 
-    after_30_minutes.export("my_audio_30_first_minutes.mp3", format="mp3")
+    # Loop through the file and extract 30-minute segments
+    while segment_start < song_length_ms:
+        # Extract segment
+        segment = song[segment_start:segment_end]
+        
+        # Export segment
+        segment_filename = f"my_audio_segment_{segment_counter}.mp3"
+        segment.export(segment_filename, format="mp3")
+        print(f"Exported: {segment_filename}")
 
-    # Check the length of the extracted segment
-    extracted_length = len(after_30_minutes) / (60 * 1000)  # Convert to minutes
-    print(f"Extracted segment length: {extracted_length} minutes")
+        # Move to the next segment
+        segment_start += thirty_minutes
+        segment_end += thirty_minutes
+        segment_counter += 1
 
 except Exception as e:
     print(f"An error occurred: {e}")
